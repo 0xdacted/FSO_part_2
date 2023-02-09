c696384a5040e373504615b89f1d64080302c99a
Red <crabmarket56@gmail.com>
2023-02-09 15:31:58 +0900

    Draft useEffect logic

--- exchange_rates/src/App.js
+++ exchange_rates/src/App.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react'
+import countriesService from './services/countries'
+
 const App = () => {
+  const [countries, setCountries] = useState(null)
   
-}
+  useEffect(() => {
+    if (countries) {
+      countriesService
+      .getAll()
+      .then(shownCountries => { 
+        setCountries(shownCountries)})
+      }}, [countries] )
+    
+      return (
+        <div>
+          find countries:
+        </div>
+      )
+  }
 
 export default App;
+
